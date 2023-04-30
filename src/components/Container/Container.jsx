1e612835f85fefd77d047f417edbec32cc4f6898
Andriy Bodnar <sokilets3@gmail.com>
2023-04-30 14:45:17 +0300

    add

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import css from './Container.module.css';
+import PropTypes from 'prop-types';
 const Container = ({ children }) => {
   return <div className={css.container}>{children}</div>;
 };
-
+Container.propTypes = {
+  children: PropTypes.node,
+};
 export default Container;
